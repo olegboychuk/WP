afe11b46d8c50d377fc7dd4f057d7e61b14b25c1
olegboychuk <oleg.boychuk@gmail.com>
2019-07-31 14:28:20 +0300

    Create script.js

---
+++ wp-content/themes/custom-theme/js/script.js
@@ -0,0 +1,23 @@
+/* jshint browser: true */
+/* global jQuery:false, MyAjax:false */
+/* global window, document, console, jQuery, MyAjax */
+/* eslint no-console: "error" */
+(function ($) {
+    "use strict";
+    
+    //After the page loads
+    $(window).load(function() {
+    
+    });
+    
+    $(window).on('resize', function(){
+    
+    });
+    
+    $(document).ready(function($) {
+    
+    });
+    
+})(jQuery);
+    
+    
